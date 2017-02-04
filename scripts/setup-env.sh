573b5780e8477e99136dfe0dc91bc3188261c392
bob walker <bob.walker@digital.cabinet-office.gov.uk>
2017-02-04 12:36:35 +0000

    set up homebrew

--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -2,6 +2,7 @@
 #script to set up things for my working environment
 
 # setup homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # install git
 
