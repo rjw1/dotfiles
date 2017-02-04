9ef451389a47510d7c8d42041a1c9330f92dc4a1
bob walker <bob.walker@digital.cabinet-office.gov.uk>
2017-02-04 12:27:49 +0000

    script to install rubies needed based on my git clones

---
+++ scripts/install-rubies.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# script to install rubies based on .ruby-version files in cloned repos
+for rubyversion in $(cat ~/git/*/.ruby-version | sort | uniq)
+do
+  rbenv install -s $rubyversion
+done
