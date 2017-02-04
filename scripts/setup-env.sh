c818cf3026b269f773039bea5810d3be38a1144d
bob walker <bob.walker@digital.cabinet-office.gov.uk>
2017-02-04 12:18:16 +0000

    Outline for a script to set up my env

---
+++ scripts/setup-env.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#script to set up things for my working environment
+
+# setup homebrew
+
+# install git
+
+# make ~/git exist
+
+# clone dotfiles
+
+# set up dotfiles
+
+# ask for homebrew github token and put that in place in .bashrc.local.d
+
+# run brew bundle
+
+# setup pathogen
+
+# generate ssh keys
+
+# output github key and then wait for confirmation of addign new key 
+# do the same for gitlab, ghe and personal git repo
+
+# run mr
+
+# install rbenv rubies
