18848d0740899b9bb78d17d4ea3f320c995fb9d0
bob walker <bob.walker@digital.cabinet-office.gov.uk>
2017-02-04 09:44:08 +0000

    Add aliases to add keys for a temporary time period

---
+++ _bashrc.d/08-add-key-aliases.sh
@@ -0,0 +1,7 @@
+alias add-github-key="ssh-add -t 86400 ~/.ssh/id_rsa.4096.github"
+alias add-ghe-key="ssh-add -t 86400 ~/.ssh/id_rsa.4096.ghe"
+alias add-personal-key="ssh-add -t 86400 ~/.ssh/id_rsa.4096.personal"
+alias add-govuk-intergration-key="ssh-add -t 3600 ~/.ssh/id_rsa.4096.govuk-intergration"
+alias add-govuk-staging-key="ssh-add -t 3600 ~/.ssh/id_rsa.4096.govuk-staging"
+alias add-govuk-production-key="ssh-add -t 300 ~/.ssh/id_rsa.4096.govuk-production"
+
