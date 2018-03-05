21433877b1a1f043ea1b6082932c05dcd8aa9bd1
bob walker <bob@randomness.org.uk>
2018-03-05 14:04:53 +0000

    use correct bash

--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -62,4 +62,6 @@ mr co
 # remove bootstrap dotfiles
 
 rm -rf ~/git/dotfiles-bs
-
+# make sure im using homebrew bash
+sudo bash -c 'echo /usr/local/bin/bash >> /etc/shells'
+chsh -s /usr/local/bin/bash
