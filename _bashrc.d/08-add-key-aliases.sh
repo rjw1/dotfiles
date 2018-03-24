89a454604bff3dc6a523b2f7ef61fadbb656806a
bob walker <bob@randomness.org.uk>
2018-03-24 13:14:40 +0000

    Add keys for 10 hours instead of 24
    Unless I'm working really late this will mean that I will have to add
    them first thing every morning.

--- _bashrc.d/08-add-key-aliases.sh
+++ _bashrc.d/08-add-key-aliases.sh
@@ -1,6 +1,6 @@
 for key in github personal gitlab dxw
 do
-  alias add-${key}-key="ssh-add -t 86400 ~/.ssh/id_rsa.4096.${key}"
+  alias add-${key}-key="ssh-add -t 36000 ~/.ssh/id_rsa.4096.${key}"
 done
 
 
