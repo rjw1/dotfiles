6765e5082205bb12e3aa74b797df1344ebde10ea
bob walker <bob@randomness.org.uk>
2019-05-05 16:07:12 +0100

    Update to bash completion 2 and run in compat mode

--- _bashrc.d/02-enable-bash-completion.sh
+++ _bashrc.d/02-enable-bash-completion.sh
@@ -1,1 +1,3 @@
-[ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion
+# [ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion
+export BASH_COMPLETION_COMPAT_DIR="/usr/local/etc/bash_completion.d"
+[[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
