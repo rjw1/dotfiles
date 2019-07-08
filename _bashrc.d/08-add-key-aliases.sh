3dd09ae5d9a1a645d0447c53564c7bb0d772abea
bob walker <bob@randomness.org.uk>
2019-07-08 16:55:19 +0100

    Add another key alias

--- _bashrc.d/08-add-key-aliases.sh
+++ _bashrc.d/08-add-key-aliases.sh
@@ -1,4 +1,4 @@
-for key in github personal gitlab dxw
+for key in github personal gitlab dxw govpress
 do
   alias add-${key}-key="cpass sshkey/${key} ; ssh-add -t 36000 ~/.ssh/id_rsa.4096.${key}"
 done
--- _bashrc.d/08-remove-key-aliases.sh
+++ _bashrc.d/08-remove-key-aliases.sh
@@ -1,4 +1,4 @@
-for key in github personal gitlab dxw
+for key in github personal gitlab dxw govpress
 do
   alias delete-${key}-key="ssh-add -d ~/.ssh/id_rsa.4096.${key}"
 done
