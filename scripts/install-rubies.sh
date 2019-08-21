4df5b37e3347c3f2cab6af39a68ae198e112d059
bob walker <bob@randomness.org.uk>
2019-08-21 09:09:58 +0100

    Quotes to appease shellcheck

--- scripts/install-rubies.sh
+++ scripts/install-rubies.sh
@@ -2,5 +2,5 @@
 # script to install rubies based on .ruby-version files in cloned repos
 for rubyversion in $(cat ~/git/*/.ruby-version | sort | uniq)
 do
-  rbenv install -s $rubyversion
+  rbenv install -s "$rubyversion"
 done
