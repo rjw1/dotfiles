f81f50c8dc4eb2a251032ca9a16553944c0008de
bob walker <bob@randomness.org.uk>
2019-08-21 09:14:06 +0100

    I no longer have GDS keys

--- scripts/ssh-key-converter.sh
+++ scripts/ssh-key-converter.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 # script to update ssh keys to new format
 
-KEYS="ghe github personal govuk-integration govuk-staging govuk-production"
+KEYS="github personal gitlab"
 
 for key in $KEYS
 do
   KEYPATH=~/.ssh/id_rsa.4096.$key
+  echo "$key"
     ssh-keygen -p -o -a 1024 -t rsa -b 4096 -f "$KEYPATH"
 done
