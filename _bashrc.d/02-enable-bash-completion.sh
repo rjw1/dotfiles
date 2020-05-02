3ed650ae27b9435938cc34a465cc2f26163acdfd
bob walker <bob@randomness.org.uk>
2020-05-02 15:11:00 +0100

    Add terraform and travis completion

--- _bashrc.d/02-enable-bash-completion.sh
+++ _bashrc.d/02-enable-bash-completion.sh
@@ -1,3 +1,9 @@
 # [ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion
 export BASH_COMPLETION_COMPAT_DIR="/usr/local/etc/bash_completion.d"
 [[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
+
+# terraform completion
+complete -C terraform terraform
+
+# added by travis gem for travis completion
+[ -f /Users/bob/.travis/travis.sh ] && source /Users/bob/.travis/travis.sh
