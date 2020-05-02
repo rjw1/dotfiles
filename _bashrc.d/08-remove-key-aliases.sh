8491be6572876f055ee669c5b4c611cefccfbf6c
bob walker <bob@randomness.org.uk>
2020-05-02 15:29:39 +0100

    Move to ed25519 keys by default
    make rsa aliases start with rsa

--- _bashrc.d/08-remove-key-aliases.sh
+++ _bashrc.d/08-remove-key-aliases.sh
@@ -1,4 +1,5 @@
 for key in github personal gitlab dxw govpress
 do
-  alias delete-${key}-key="ssh-add -d ~/.ssh/id_rsa.4096.${key}"
+  alias rsa-delete-${key}-key="ssh-add -d ~/.ssh/id_rsa.4096.${key}"
+  alias delete-${key}-key="ssh-add -d ~/.ssh/id_ed25519.${key}"
 done
