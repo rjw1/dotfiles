519afc3ba00926dc0a60a2de348e0c8eea096d5f
bob walker <bob@randomness.org.uk>
2020-09-29 18:09:56 +0100

    Update pass config
    Make default password size 32 and allow the local extensions folder

--- _bashrc.d/13-pass.sh
+++ _bashrc.d/13-pass.sh
@@ -2,4 +2,5 @@ alias cpass="pass -c"
 alias gpass="pass generate -c"
 alias totp="pass otp -c"
 complete -o filenames -F _pass cpass gpass totp
-export PASSWORD_STORE_GENERATED_LENGTH='16'
+export PASSWORD_STORE_GENERATED_LENGTH='32'
+export PASSWORD_STORE_ENABLE_EXTENSIONS=true
