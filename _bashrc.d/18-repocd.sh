0cdd3d85fbb921768b7507a256d5cd932a91f77d
bob walker <bob@randomness.org.uk>
2020-12-13 15:51:58 +0000

    Add a repo alias with compeletion
    Add an alias which can jump to git repos in `~/git/` from anywhere.
    
    e.g `repo rjw1 OpenGuides`
    
    also enable tab completion so I can save typing.

---
+++ _bashrc.d/18-repocd.sh
@@ -0,0 +1,29 @@
+repo()  { cd ~/git/$1/$2; }
+_repo_complete()
+{
+  local cur prev
+
+  COMPREPLY=()
+  cur=${COMP_WORDS[COMP_CWORD]}
+  prev=${COMP_WORDS[COMP_CWORD-1]}
+
+  if [ $COMP_CWORD -eq 1 ]; then
+    COMPREPLY=( $(compgen -W "$(basename ~/git/*)" -- $cur) )
+  elif [ $COMP_CWORD -eq 2 ]; then
+    case "$prev" in
+      "rjw1")
+        COMPREPLY=( $(compgen -W "$(basename ~/git/rjw1/*)" -- $cur) )
+        ;;
+      "dxw")
+        COMPREPLY=( $(compgen -W "$(basename ~/git/dxw/*)" -- $cur) )
+        ;;
+      "alphagov")
+        COMPREPLY=( $(compgen -W "$(basename ~/git/alphagov/*)" -- $cur) )
+        ;;
+      *)
+        ;;
+    esac
+  fi
+}
+  
+complete -F _repo_complete repo
