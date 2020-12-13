75b075a82bbae727274e3a57cc1b0788d6f747af
bob walker <bob@randomness.org.uk>
2020-12-13 15:51:03 +0000

    Use brew curl

---
+++ _bashrc.d/17-curl.sh
@@ -0,0 +1,6 @@
+export PATH="/usr/local/opt/curl/bin:$PATH"
+
+export LDFLAGS="-L/usr/local/opt/curl/lib"
+export CPPFLAGS="-I/usr/local/opt/curl/include"
+
+export PKG_CONFIG_PATH="/usr/local/opt/curl/lib/pkgconfig"
