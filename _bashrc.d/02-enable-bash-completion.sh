4ffd7bd72a6b1cec5bdc3b5116f9e3405c4e2215
bob walker <bob@randomness.org.uk>
2022-12-24 14:32:22 +0000

    Explicitly source bash completion for git
    For some reason I had to do this.

--- _bashrc.d/02-enable-bash-completion.sh
+++ _bashrc.d/02-enable-bash-completion.sh
@@ -2,6 +2,7 @@
 export BASH_COMPLETION_COMPAT_DIR="/usr/local/etc/bash_completion.d"
 [[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
 
+. /usr/local/etc/bash_completion.d/git-completion.bash
 # terraform completion
 complete -C terraform terraform
 
