4e87740eea0a20bcdf61971d54b629b49a6496e2
bob <bob@randomness.org.uk>
2022-12-29 12:15:50 +0000

    Change path of git repos

--- scripts/install-rubies.sh
+++ scripts/install-rubies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # script to install rubies based on .ruby-version files in cloned repos
-for rubyversion in $(cat ~/git/*/.ruby-version | sort | uniq)
+for rubyversion in $(cat ~/git/dxw/*/.ruby-version ~/git/rjw1/*/.ruby-version  | sort | uniq)
 do
   rbenv install -s "$rubyversion"
 done
