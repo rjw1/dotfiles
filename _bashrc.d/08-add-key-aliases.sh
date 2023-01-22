77f7460616438a03d821f601727d3d68eed87aa3
bob walker <bob@randomness.org.uk>
2023-01-22 15:50:52 +0000

    Make ssh key alias use pass via script
    Setting SSH_ASKPASS_REQUIRE=force means that ssh-add will always use what
    SSH_ASKPASS is set to instead of making decisions based on how it is being run.
    
    SSH_ASKPASS can be any script which returns a password. This cant be passed
    options. If you pass options you will get a less than helpful error message
    about there being "No such file or directory".
    
    Therefore we currently need a script per key.

--- _bashrc.d/08-add-key-aliases.sh
+++ _bashrc.d/08-add-key-aliases.sh
@@ -1,6 +1,6 @@
 for key in github personal gitlab dxw govpress
 do
-  alias add-${key}-key="cpass sshkey/${key} ; ssh-add -t 36000 ~/.ssh/id_ed25519.${key}"
+  alias add-${key}-key="SSH_ASKPASS_REQUIRE=force SSH_ASKPASS=\"/Users/bob/bin/${key}-sshkey-pass\" ssh-add -t 36000 ~/.ssh/id_ed25519.${key}"
   alias rsa-add-${key}-key="cpass sshkey/${key} ; ssh-add -t 36000 ~/.ssh/id_rsa.4096.${key}"
 done
 
