5b4b4f364d84945043ebbb9388a3b0d81d0a46cf
bob walker <bob@randomness.org.uk>
2025-01-10 16:33:45 +0000

    Add new mail machine as update alias

--- _bashrc.d/15-update-ashbrook.sh
+++ _bashrc.d/15-update-ashbrook.sh
@@ -1,1 +1,3 @@
 alias update-ashbrook="ssh -A ashbrook.default.rjw1.uk0.bigv.io git pull"
+alias update-sympl-config='ssh -A sympl@mail01rjw.vs.mythic-beasts.com "cd /srv; git pull"'
+alias update-sympl-dns='ssh -A mail01rjw.vs.mythic-beasts.com "sudo /root/BytemarkDNS/run-dns.sh"'
