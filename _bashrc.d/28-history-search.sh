50951166e969f01b570f6e52b16fe6c7b64e8815
bob walker <bob@randomness.org.uk>
2025-01-10 16:36:31 +0000

    Make up and down match partial history search

---
+++ _bashrc.d/28-history-search.sh
@@ -0,0 +1,4 @@
+# up and down arrow keys to search through history from command line
+# https://askubuntu.com/questions/59846/bash-history-search-partial-up-arrow#59855
+bind '"\e[A": history-search-backward'
+bind '"\e[B": history-search-forward'
