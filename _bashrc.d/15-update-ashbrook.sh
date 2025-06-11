d84bade49314c79f2ba7d8d82c5de14e030f4695
bob walker <bob@randomness.org.uk>
2025-06-11 15:46:08 +0100

    Remove update-ashbrook alias since that server is no longer used.

--- _bashrc.d/15-update-ashbrook.sh
+++ _bashrc.d/15-update-ashbrook.sh
@@ -1,4 +1,3 @@
-alias update-ashbrook="ssh -A ashbrook.default.rjw1.uk0.bigv.io git pull"
 alias update-sympl-config='ssh -A sympl@mail01rjw.vs.mythic-beasts.com "cd /srv; git pull"'
 alias update-sympl-dns='ssh -A mail01rjw.vs.mythic-beasts.com "sudo /root/BytemarkDNS/run-dns.sh"'
 alias spamtrain-sympl='ssh sympl@mail01rjw.vs.mythic-beasts.com "/usr/bin/sa-learn --spam /srv/randomness.org.uk/mailboxes/bob/Maildir/.spamtrain"'
