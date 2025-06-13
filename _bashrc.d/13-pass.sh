0532068ce6e052d4f15d343a94559960ad4da038
bob walker <bob@randomness.org.uk>
2025-06-13 15:04:42 +0100

    Add an alias to display the OTP URI in a QR code format
    This helps to add things to other devices easily.

--- _bashrc.d/13-pass.sh
+++ _bashrc.d/13-pass.sh
@@ -1,6 +1,7 @@
 alias cpass="pass -c"
 alias gpass="pass generate -c"
 alias totp="pass otp -c"
-complete -o filenames -F _pass cpass gpass totp
+alias qpass="pass otp uri -q"
+complete -o filenames -F _pass cpass gpass qpass totp
 export PASSWORD_STORE_GENERATED_LENGTH='32'
 export PASSWORD_STORE_ENABLE_EXTENSIONS=true
