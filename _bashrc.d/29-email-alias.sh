e2057c9dda9787dcc35379aa44260eea7b1cefcc
bob walker <bob@randomness.org.uk>
2025-09-03 15:54:38 +0100

    Add an alias to start a new pine session if one doesn't exist

--- _bashrc.d/29-email-alias.sh
+++ _bashrc.d/29-email-alias.sh
@@ -1,1 +1,2 @@
 alias alpine="ssh mail01rjw.vs.mythic-beasts.com rm /home/bob/.opener.sock; ssh mail01rjw.vs.mythic-beasts.com -t tmux attach-session -d -t pine"
+alias start-pine-session="ssh mail01rjw.vs.mythic-beasts.com rm /home/bob/.opener.sock; ssh mail01rjw.vs.mythic-beasts.com -t tmux new-session -s pine -d alpine"
