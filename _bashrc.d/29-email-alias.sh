896228b1b694ca166453abf436a8789d007e106e
bob walker <bob@randomness.org.uk>
2025-09-03 15:53:35 +0100

    Change Server I read email on to the new mail server

--- _bashrc.d/29-email-alias.sh
+++ _bashrc.d/29-email-alias.sh
@@ -1,1 +1,1 @@
-alias alpine="ssh pi-01 rm /home/bob/.opener.sock; ssh pi-01 -t tmux attach-session -d -t pine"
+alias alpine="ssh mail01rjw.vs.mythic-beasts.com rm /home/bob/.opener.sock; ssh mail01rjw.vs.mythic-beasts.com -t tmux attach-session -d -t pine"
