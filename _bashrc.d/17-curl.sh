4c58fa68248e99c72e39bb3dd1f24385e4eb7a2a
bob walker <bob@randomness.org.uk>
2026-09-05 23:17:25 +0100

    Add an alias to time curl requests.

--- _bashrc.d/17-curl.sh
+++ _bashrc.d/17-curl.sh
@@ -4,3 +4,4 @@ export LDFLAGS="-L/opt/homebrew/opt/curl/lib"
 export CPPFLAGS="-I/opt/homebrew/opt/curl/include"
 
 export PKG_CONFIG_PATH="/opt/homebrew/opt/curl/lib/pkgconfig"
+alias curltime='curl -w "\nDNS: %{time_namelookup}s\nTCP: %{time_connect}s\nTLS: %{time_appconnect}s\nTTFB: %{time_starttransfer}s\nTotal: %{time_total}s\n" -o /dev/null -s'
